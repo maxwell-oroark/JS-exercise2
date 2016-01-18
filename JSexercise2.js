3883b6004dc623346e89af9e1ee53d62044966e3
Student <student@RU-004.local>
2016-01-18 11:52:18 -0700

    second JS exercise

---
+++ JSexercise2.js
@@ -0,0 +1,47 @@
+// 1
+
+for (var d = 0; d < 550; d += 100){
+	console.log(d);
+}
+
+
+//2
+
+for (var x = 1; x < 64; x += x){
+	console.log(x);
+}
+
+//3
+
+for (var y = 0; y < 4; y++){
+	console.log(y);
+	console.log(y);
+	console.log(y);
+}
+
+//4
+
+for (var s = 0; s < 11; s += 2){
+	console.log(s)
+}
+
+//5
+
+for (var s = 3; s < 16; s += 3){
+	console.log(s)
+}
+
+//6
+
+for (var s = 10; s > -1; s +- 1){
+	console.log(s)
+}
+
+//7
+
+for (var i = 0; x < 4; x++){
+	console.log(0);
+	console.log(1);
+	console.log(2);
+	console.log(3);
+}
